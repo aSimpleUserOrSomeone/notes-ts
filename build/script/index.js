f396cd28b6658b41b9a158ee4e9e1d9ed70b101f
hardToPickANameIsntIt <37453773+aSimpleUserOrSomeone@users.noreply.github.com>
2022-10-05 20:10:44 +0200

    Initial commit
    Changes from lesson

---
+++ build/script/index.js
@@ -0,0 +1,12 @@
+var myC = /** @class */ (function () {
+    function myC(X, Y) {
+        if (X === void 0) { X = 0; }
+        if (Y === void 0) { Y = 0; }
+        this.x = X;
+        this.y = Y;
+    }
+    return myC;
+}());
+var c = new myC(1, 24);
+console.log(c);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+class myC {
+	x: number
+	y: number
+	constructor(X = 0, Y = 0) {
+		this.x = X
+		this.y = Y
+	}
+}
+
+const c = new myC(1, 24)
+console.log(c)

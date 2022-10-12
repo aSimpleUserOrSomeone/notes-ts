70bbb7ea6b1d8c98bca5a714e0f7c9ab1527b5be
hardToPickANameIsntIt <37453773+aSimpleUserOrSomeone@users.noreply.github.com>
2022-10-12 17:27:49 +0200

    Styling + class
    Note class plus html and css work

--- build/script/index.js
+++ build/script/index.js
@@ -1,12 +1,42 @@
-var myC = /** @class */ (function () {
-    function myC(X, Y) {
-        if (X === void 0) { X = 0; }
-        if (Y === void 0) { Y = 0; }
-        this.x = X;
-        this.y = Y;
+var Note = /** @class */ (function () {
+    function Note() {
+        this.posX = 0;
+        this.posY = 0;
+        this.posZ = Note.currentCount;
+        Note.totalCount++;
+        Note.currentCount++;
+        this.posZ = Note.currentCount + 1;
+        this.instantiate();
+        Note.allNotes.push(this);
     }
-    return myC;
+    Note.prototype.updateZ = function () {
+        Note.allNotes.forEach(function (el, i) { return (el.posZ = i); });
+    };
+    Note.sortZ = function () {
+        //sorting does nothing
+        if (Note.allNotes.length < 2)
+            return;
+        //biggest posZ (the most in front) is sorted to the back of the array
+        Note.allNotes.sort(function (a, b) {
+            if (a.posZ > b.posZ)
+                return 1;
+            else
+                return 0;
+        });
+    };
+    Note.prototype.instantiate = function () {
+        var myNote = document.createElement('div');
+        return myNote;
+    };
+    Note.prototype.destroy = function () {
+        Note.currentCount--;
+        this.instance.remove();
+        //removes this specific note from the array to stop keeping track of it
+        Note.allNotes.splice(Note.allNotes.indexOf(this), 1);
+    };
+    Note.totalCount = 0;
+    Note.currentCount = 0;
+    Note.allNotes = [];
+    return Note;
 }());
-var c = new myC(1, 24);
-console.log(c);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,52 @@
-class myC {
-	x: number
-	y: number
-	constructor(X = 0, Y = 0) {
-		this.x = X
-		this.y = Y
+class Note {
+	static allCount: number = 0
+	static nowCount: number = 0
+	static allNotes: Array<Note> = []
+
+	public posX: number
+	public posY: number
+	private posZ: number //minimum posZ is 0 NOT 1 (represents indexZ)
+	public instance: HTMLElement
+
+	public updateZ() {
+		Note.allNotes.forEach((el, i) => (el.posZ = i))
+	}
+
+	static sortZ() {
+		//sorting does nothing
+		if (Note.allNotes.length < 2) return
+
+		//biggest posZ (the most in front) is sorted to the back of the array
+		Note.allNotes.sort((a, b) => {
+			if (a.posZ > b.posZ) return 1
+			else return 0
+		})
+	}
+
+	private instantiate() {
+		var myNote = document.createElement('div')
+
+		return myNote
+	}
+
+	constructor() {
+		this.posX = 0
+		this.posY = 0
+		this.posZ = Note.nowCount
+
+		Note.allCount++
+		Note.nowCount++
+		this.posZ = Note.nowCount + 1
+
+		this.instantiate()
+		Note.allNotes.push(this)
+	}
+
+	public destroy() {
+		Note.nowCount--
+		this.instance.remove()
+
+		//removes this specific note from the array to stop keeping track of it
+		Note.allNotes.splice(Note.allNotes.indexOf(this), 1)
 	}
 }
-
-const c = new myC(1, 24)
-console.log(c)

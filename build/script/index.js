0a60da715e1bf309b5d44d5506c93a8348867bbc
hardToPickANameIsntIt <37453773+aSimpleUserOrSomeone@users.noreply.github.com>
2022-10-13 20:01:52 +0200

    Notes and counter improvements
    Notes highlight, overlap and move correctly.
    Except going outside borders but that can be fixed. Counters also work

--- build/script/index.js
+++ build/script/index.js
@@ -1,5 +1,18 @@
 var Note = /** @class */ (function () {
     function Note() {
+        var _this = this;
+        this.SortZ = function () {
+            //sorting does nothing
+            if (Note.allNotes.length < 2)
+                return;
+            //push current note to the back of the array
+            Note.allNotes.push(Note.allNotes.splice(Note.allNotes.indexOf(_this), 1)[0]);
+            //update posZ on all Notes and their zIndex
+            Note.allNotes.forEach(function (el, i) {
+                el.posZ = i;
+                el.instance.style.zIndex = el.posZ.toString();
+            });
+        };
         this.posX = 0;
         this.posY = 0;
         this.posZ = Note.nowCount;
@@ -9,58 +22,76 @@ var Note = /** @class */ (function () {
         this.Instantiate();
         Note.allNotes.push(this);
     }
-    Note.SortZ = function () {
-        //sorting does nothing
-        if (Note.allNotes.length < 2)
-            return;
-        //biggest posZ (the most in front) is sorted to the back of the array
-        Note.allNotes.sort(function (a, b) {
-            if (a.posZ > b.posZ)
-                return 1;
-            else
-                return 0;
-        });
-    };
-    Note.prototype.UpdateZ = function () {
-        Note.allNotes.forEach(function (el, i) { return (el.posZ = i); });
+    Note.prototype.UpdateCountHTML = function () {
+        var allCountP = document.querySelector('.counter-all-p');
+        var nowCountP = document.querySelector('.counter-now-p');
+        allCountP.textContent = "Przebieg: ".concat(Note.allCount);
+        nowCountP.textContent = "Na lod\u00F3wce: ".concat(Note.nowCount);
     };
     Note.prototype.ActivateMe = function () {
-        Note.allNotes.forEach(function (el) {
-            el.instance.classList.remove('active');
-        });
+        if (!this.instance)
+            return;
         this.instance.classList.add('active');
+        this.SortZ();
     };
-    Note.prototype.MoveMe = function () {
-        var pos1, pos2, pos3, pos4;
+    Note.prototype.DeactivateMe = function () {
+        if (this.instance.classList.contains('active')) {
+            this.instance.classList.remove('active');
+        }
+    };
+    Note.prototype.MoveMe = function (element) {
+        var _this = this;
+        var pos1, pos2;
         var dragMouseDown = function (e) {
             e = e || window.event;
-            e.preventDefault();
-            pos3 = e.clientX;
-            pos4 = e.clientY;
+            _this.posX = e.clientX;
+            _this.posY = e.clientY;
+            element.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
         };
-        this.instance.onmousedown = dragMouseDown;
+        var elementDrag = function (e) {
+            e = e || window.event;
+            pos1 = _this.posX - e.clientX;
+            pos2 = _this.posY - e.clientY;
+            _this.posX = e.clientX;
+            _this.posY = e.clientY;
+            _this.ActivateMe();
+            _this.instance.style.top = _this.instance.offsetTop - pos2 + 'px';
+            _this.instance.style.left = _this.instance.offsetLeft - pos1 + 'px';
+        };
+        var closeDragElement = function () {
+            _this.DeactivateMe();
+            document.onmouseup = null;
+            document.onmousemove = null;
+        };
+        element.onmousedown = dragMouseDown;
     };
     Note.prototype.Instantiate = function () {
         var _this = this;
         this.instance = document.createElement('div');
         this.instance.classList.add('note');
-        var textField = document.createElement('p');
-        textField.classList.add('textfield-p');
-        textField.textContent = 'Note';
-        this.instance.append(textField);
+        var titleH3 = document.createElement('h3');
+        titleH3.classList.add('title-h3');
+        titleH3.textContent = 'Title';
+        this.instance.append(titleH3);
         var closeBtn = document.createElement('button');
         closeBtn.classList.add('close-btn');
         closeBtn.addEventListener('click', function () {
             _this.Destroy();
         });
         this.instance.append(closeBtn);
+        var textField = document.createElement('p');
+        textField.classList.add('textfield-p');
+        textField.textContent = 'Note';
+        this.instance.append(textField);
         document.querySelector('main').append(this.instance);
-        this.instance.addEventListener('click', function () {
-            _this.ActivateMe();
-        });
+        textField.onclick = this.ActivateMe;
+        this.MoveMe(textField);
+        this.UpdateCountHTML();
     };
     Note.prototype.Destroy = function () {
         Note.nowCount--;
+        this.UpdateCountHTML();
         this.instance.remove();
         //removes this specific note from the array to stop keeping track of it
         Note.allNotes.splice(Note.allNotes.indexOf(this), 1);
@@ -71,7 +102,7 @@ var Note = /** @class */ (function () {
     return Note;
 }());
 var addNewNote = function () {
-    var newNote = new Note();
+    new Note();
 };
 var newNoteBtn = document.querySelector('.new-note-btn');
 var setup = function () {
--- src/index.ts
+++ src/index.ts
@@ -5,53 +5,84 @@ class Note {
 
 	public posX: number
 	public posY: number
-	private posZ: number //minimum posZ is 0 NOT 1 (represents indexZ)
+	public posZ: number //minimum posZ is 0 NOT 1 (represents indexZ)
 	public instance: HTMLElement
 
-	static SortZ() {
+	private UpdateCountHTML() {
+		const allCountP = document.querySelector('.counter-all-p')
+		const nowCountP = document.querySelector('.counter-now-p')
+
+		allCountP.textContent = `Przebieg: ${Note.allCount}`
+		nowCountP.textContent = `Na lodówce: ${Note.nowCount}`
+	}
+
+	public SortZ = () => {
 		//sorting does nothing
 		if (Note.allNotes.length < 2) return
 
-		//biggest posZ (the most in front) is sorted to the back of the array
-		Note.allNotes.sort((a, b) => {
-			if (a.posZ > b.posZ) return 1
-			else return 0
-		})
-	}
+		//push current note to the back of the array
+		Note.allNotes.push(Note.allNotes.splice(Note.allNotes.indexOf(this), 1)[0])
 
-	public UpdateZ() {
-		Note.allNotes.forEach((el, i) => (el.posZ = i))
+		//update posZ on all Notes and their zIndex
+		Note.allNotes.forEach((el, i) => {
+			el.posZ = i
+			el.instance.style.zIndex = el.posZ.toString()
+		})
 	}
 
 	private ActivateMe() {
-		Note.allNotes.forEach((el) => {
-			el.instance.classList.remove('active')
-		})
+		if (!this.instance) return
 		this.instance.classList.add('active')
+		this.SortZ()
 	}
 
-	private MoveMe() {
-		var pos1, pos2, pos3, pos4
+	private DeactivateMe() {
+		if (this.instance.classList.contains('active')) {
+			this.instance.classList.remove('active')
+		}
+	}
+
+	private MoveMe(element: HTMLElement) {
+		var pos1, pos2
 
 		const dragMouseDown = (e) => {
 			e = e || window.event
-			e.preventDefault()
 
-			pos3 = e.clientX
-			pos4 = e.clientY
+			this.posX = e.clientX
+			this.posY = e.clientY
+			element.onmouseup = closeDragElement
+			document.onmousemove = elementDrag
 		}
 
-		this.instance.onmousedown = dragMouseDown
+		const elementDrag = (e) => {
+			e = e || window.event
+			pos1 = this.posX - e.clientX
+			pos2 = this.posY - e.clientY
+			this.posX = e.clientX
+			this.posY = e.clientY
+			this.ActivateMe()
+
+			this.instance.style.top = this.instance.offsetTop - pos2 + 'px'
+			this.instance.style.left = this.instance.offsetLeft - pos1 + 'px'
+		}
+
+		const closeDragElement = () => {
+			this.DeactivateMe()
+			document.onmouseup = null
+			document.onmousemove = null
+		}
+
+		element.onmousedown = dragMouseDown
 	}
 
 	private Instantiate() {
 		this.instance = document.createElement('div')
 		this.instance.classList.add('note')
 
-		const textField: HTMLElement = document.createElement('p')
-		textField.classList.add('textfield-p')
-		textField.textContent = 'Note'
-		this.instance.append(textField)
+		const titleH3: HTMLElement = document.createElement('h3')
+		titleH3.classList.add('title-h3')
+		titleH3.textContent = 'Title'
+		this.instance.append(titleH3)
 
 		const closeBtn: HTMLElement = document.createElement('button')
 		closeBtn.classList.add('close-btn')
@@ -60,10 +91,16 @@ class Note {
 		})
 		this.instance.append(closeBtn)
 
-		document.querySelector('main').append(this.instance)
+		const textField: HTMLElement = document.createElement('p')
+		textField.classList.add('textfield-p')
+		textField.textContent = 'Note'
+		this.instance.append(textField)
 
-		this.instance.click = this.ActivateMe
-		this.MoveMe()
+		document.querySelector('main').append(this.instance)
+		textField.onclick = this.ActivateMe
+		this.MoveMe(textField)
+
+		this.UpdateCountHTML()
 	}
 
 	constructor() {
@@ -81,6 +118,7 @@ class Note {
 
 	public Destroy() {
 		Note.nowCount--
+		this.UpdateCountHTML()
 		this.instance.remove()
 
 		//removes this specific note from the array to stop keeping track of it
@@ -89,7 +127,7 @@ class Note {
 }
 
 const addNewNote = () => {
-	const newNote: Note = new Note()
+	new Note()
 }
 
 const newNoteBtn: HTMLElement = document.querySelector('.new-note-btn')
